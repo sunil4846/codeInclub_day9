5b6cd0787c0b70fc9319bdf2d9c3a7d73091fd2e
sunil4846 <sunilrajput4846@gmail.com>
2022-01-04 19:39:46 +0530

    Added function Refactor feature

--- calculator.sh
+++ calculator.sh
@@ -1,7 +1,46 @@
+function sum() {
+	sum=$(($a+$b))
+	echo "Sum is $sum"
+}
+
+function sub() {
+	sub=$(($a-$b))
+	echo "Sub is $sub"
+}
+
+function mul() {
+	mul=$(($a*$b))
+	echo "Mul is $mul"
+}
+
+function div() {
+	div=$(($a/$b))
+	echo "Div is $div"
+}
+
 echo "Enter a 1st number"
 read a
 echo "Enter a 2nd number"
 read b
-div=$(($a/$b))
-echo "Division is $div"
-
+echo "choose 1 for Addition"
+echo "choose 2 for Substraction"
+echo "choose 3 for Multiplication"
+echo "choose 4 for Division"
+read option
+case $option in
+	1)
+		sum
+		;;
+	2)
+		sub
+		;;
+	3)
+		mul
+		;;
+	4)
+		div
+		;;
+	*)
+		echo "valid input is incorrect"
+		;;
+esac
